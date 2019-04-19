c72d820fd3a5ba3ff3288d88dc368a223d3d2d2e
mloo3 <slayer71432@gmail.com>
2019-04-19 13:06:52 -0400

    updated cmake and config pertaining to fpga

--- AOCLUtils/inc/aocl_utils.h
+++ AOCLUtils/inc/aocl_utils.h
@@ -23,10 +23,10 @@
 
 #ifndef AOCL_UTILS_H
 #define AOCL_UTILS_H
-
-#include "AOCLUtils/opencl.h"
-#include "AOCLUtils/scoped_ptrs.h"
-#include "AOCLUtils/options.h"
+// inc/
+#include "inc/opencl.h"
+#include "inc/scoped_ptrs.h"
+#include "inc/options.h"
 
 #endif
 
